286eca71617ba4cbf6e3d46182435921dddebc88
Arman Z. Papyan <Arman.Z.Papyan@ad.ucom.am>
2023-10-06 19:04:41 +0400

    edit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import  {tempMovieData,tempWatchedData} from "./data";
+// import App from './App';
+// import  {tempMovieData,tempWatchedData} from "./data";
+import StarRating from "./components/Starating/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App
+//         tempMovieData={tempMovieData}
+//         tempWatchedData={tempWatchedData}
+//     />
+// );
 root.render(
-    <App
-        tempMovieData={tempMovieData}
-        tempWatchedData={tempWatchedData}
-    />
+    <StarRating />
 );
 
 
