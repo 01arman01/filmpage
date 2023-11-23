9cc1c970edcad3fc5aecc9b052bed8a16d1fb9de
Arman Z. Papyan <Arman.Z.Papyan@ad.ucom.am>
2023-11-23 17:16:15 +0400

    test

--- src/App.js
+++ src/App.js
@@ -13,63 +13,77 @@ import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
 
 const key = '3fb280c7'
 // const query = 'interstellar'
-const query = 'oiurygt;iw'
+// const query = 'interstellar'
 
 function App({tempWatchedData}) {
     const [tempMovieData, setTempMovieData] = useState([])
     const [isLoaded, setIsLoaded] = useState(false)
-    const [error,setError] = useState('')
+    const [error, setError] = useState('')
+    const [query, setQuery] = useState('')
+
     useEffect(
         function () {
-             async function fetchMovies() {
-                 try {
-                     setIsLoaded(true)
-                     const res = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=${query}`)
-                     if (!res.ok) {
-                         throw new Error('Something went wrong with fetching  movies')
-                     }
-                     const data = await res.json()
-                     if(data.Response === 'False'){
-                        throw  new Error('Move not found')
-                     }
+            async function fetchMovies() {
+                console.log(query)
+                try {
+                    setIsLoaded(true)
+                    setError('')
 
-                     setTempMovieData(data.Search)
-                     console.log(data)
-                     // setIsLoaded(false)
-                 } catch (err) {
-                     console.error(err)
-                     setError(err.message)
-                 } finally {
-                     setIsLoaded(false)
-                 }
-             }
+                    const res = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=${query}`)
+                    if (!res.ok) {
+                        throw new Error('Something went wrong with fetching  movies')
+                    }
+                    const data = await res.json()
+                    console.log(data)
+                    if (data.Response === 'False') {
+                        throw new Error('Move not found')
+                    }
+                    setTempMovieData(data.Search)
+                    console.log(data)
+                    // setIsLoaded(false)
+                } catch (err) {
+                    console.error(err)
+                    setError(err.message)
+                } finally {
+
+                    setIsLoaded(false)
+                }
+            }
+
+            if(query.length < 3){
+                setError('')
+                setTempMovieData([])
+                return;
+            }
+
             fetchMovies()
-             },[])
-
+        }, [query]
+    )
     // fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=interstellar`).then(res=>res.json()).then(data=> setTempMovieData(data.Search))
 
     return (
         <div className="App">
             <Header>
                 <Logo/>
-                <Search/>
+                <Search query={query} setQuery={setQuery}/>
                 <NumResults numListMoves={tempMovieData.length}/>
             </Header>
             <main>
                 <ListComponent>
-                   {/* {isLoaded? <Loader/> : <div>*/}
-                   {/*    {*/}
-                   {/*        tempMovieData.map(item => {*/}
-                   {/*            return <MoveItem key={item.imdbID} item={item}/>*/}
-                   {/*        })*/}
-                   {/*    }*/}
-                   {/*</div>}*/}
+                    {/* {isLoaded? <Loader/> : <div>*/}
+                    {/*    {*/}
+                    {/*        tempMovieData.map(item => {*/}
+                    {/*            return <MoveItem key={item.imdbID} item={item}/>*/}
+                    {/*        })*/}
+                    {/*    }*/}
+                    {/*</div>}*/}
 
-                    {isLoaded && <Loader />}
-                    {!isLoaded && !error &&  tempMovieData.map(item => {
-                               return <MoveItem key={item.imdbID} item={item}/> })}
+                    {isLoaded && <Loader/>}
+                    {!isLoaded && !error && tempMovieData.map(item => {
+                        return <MoveItem key={item.imdbID} item={item}/>
+                    })}
                     {
-                        error && <ErrorMessage message={error}  />
+                        error && <ErrorMessage message={error}/>
                     }
 
                 </ListComponent>
--- src/components/Header/Saerch.js
+++ src/components/Header/Saerch.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import s from "./Header.module.css";
 
-function Search(props) {
-    const [SearchValue, setSearchValue] = useState('')
+function Search({query,setQuery}) {
+    // const [SearchValue, setSearchValue] = useState('')
 
     return (
         <div><input type='text'
                     className={s.search}
                     placeholder='Search movies...'
-                    value={SearchValue}
-                    onChange={e => setSearchValue(e.target.value)}
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
         /></div>
     );
 }
